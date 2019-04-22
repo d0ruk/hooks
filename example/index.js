b24868d2381862c7f2ea2a49b992c703fc6134b9
Doruk Kutlu <me@doruk.me>
2019-04-22 05:51:13 +0300

    add links to ext libs in examples/

--- example/index.js
+++ example/index.js
@@ -15,23 +15,46 @@ import Location from "./location";
 
 log.info("lib", hooks);
 
-const App = () => (
-  <main>
-    <Hanger />
-    <hr />
-    <Form />
-    <hr />
-    <Portal />
-    <hr />
-    <Intersection />
-    <hr />
-    <Fetch />
-    <hr />
-    <Media />
-    <hr />
-    <Location />
-    <hr />
-  </main>
-);
+const App = () => {
+  return (
+    <main>
+      <h2>
+        <a href="https://github.com/kitze/react-hanger">react-hanger</a>
+      </h2>
+      <Hanger />
+      <hr />
+      <h2>
+        <a href="https://github.com/wsmd/react-use-form-state">
+          react-use-form-state
+        </a>
+      </h2>
+      <Form />
+      <hr />
+      <Portal />
+      <hr />
+      <h2>
+        <a href="https://github.com/bsonntag/react-use-intersection-observer">
+          react-use-intersection-observer
+        </a>
+      </h2>
+      <Intersection />
+      <hr />
+      <h2>
+        <a href="https://github.com/ilyalesik/react-fetch-hook">
+          react-fetch-hook
+        </a>
+      </h2>
+      <Fetch />
+      <hr />
+      <h2>
+        <a href="https://github.com/streamich/use-media">use-media</a>
+      </h2>
+      <Media />
+      <hr />
+      <Location />
+      <hr />
+    </main>
+  );
+};
 
 render(<App />, document.getElementById("root"));
